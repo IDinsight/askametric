354b9992a1d80c9e5851911a4e7db4c92d51a750
Poornima Ramesh <poornimaramesh1995@gmail.com>
2024-12-10 11:53:41 +0530

    Get DB description and suggested questions (#11)
    Reviewer:
    Primary reviewer: @suzinyou
    Secondary reviewer: @ziakhan04
    
    Estimate: ~15 mins
    
    ---
    
    ### Goal
    Add functionality for getting a user-parsable description and suggested
    questions
    
    ### Changes
    - Added a new module inside `query_processor` called `db_descriptor`
    - Used a single class called DBDescriptor to get and cache descriptions
    + suggested questions for all databases (hard coded prompts / steps
    after checking how the crewai agents do this)
    - Added a cell in the demo notebook for this
    
    ## How has this been tested?
    Run the demo notebook (I tested with the TN COVID, ESA GBV and Morocco
    Afrobarometer DBs)
    
    ## TODOS:
    - I tested functionality with and without chat history -- doesn't seem
    liek chat history makes a huge difference on the test DBs for now, so I
    took it out. We can put it back in after we figure out how we're going
    to do logging
    
    
    ## To-do before merge
    - [x] My code follows the style guidelines of this project
    - [x] I have reviewed my own code to ensure good quality
    - [x] I have tested the functionality of my code to ensure it works as
    intended
    - [x] I have resolved merge conflicts
    - [x] I have updated the requirements (if applicable)
    - [x] I have updated affected documentation (if applicable)

---
+++ askametric/query_processor/db_descriptor/description_generator.py
@@ -0,0 +1,153 @@
+from .descriptor_prompts import (
+    generate_description_prompt,
+    generate_suggested_questions_prompt,
+)
+from ...utils import _ask_llm_json, setup_logger, get_log_level_from_str
+from ..tools import get_tools, track_time
+import json
+from sqlalchemy.ext.asyncio import AsyncSession
+from cachetools import TTLCache
+from aiocache import cached
+
+_db_descriptor_instance = None
+
+
+class DatabaseDescriptor:
+    """Generates helpful descriptions for the database"""
+
+    def __init__(
+        self, llm: str = "gpt-4o", temperature: float = 0.1, log_level: str = "INFO"
+    ):
+        """
+        Initialize the DatabaseDescriptor class.
+
+        Args:
+            llm (str): The name of the LLM model to use. Defaults to "gpt-4o".
+            temperature (float): The temperature to use when generating descriptions.
+                Defaults to 0.1.
+            log_level (str): The log level to use. Defaults to "INFO".
+        """
+        self.llm = llm
+        self.temperature = temperature
+
+        self.logger = setup_logger("db_descriptor", get_log_level_from_str(log_level))
+
+        self.tools = get_tools()
+        self._description_cache: TTLCache = TTLCache(maxsize=100, ttl=60 * 60 * 24)
+        self._description_cache["db_description"] = {}
+        self._description_cache["suggested_questions"] = {}
+
+    @track_time(create_class_attr="timings")
+    @cached(ttl=60 * 60 * 24)
+    async def generate_db_description(
+        self,
+        asession: AsyncSession,
+        metric_db_id: str,
+        sys_message: str,
+        table_description: str,
+        column_description: str = "",
+    ) -> str:
+        """
+        Generate a database description.
+
+        Args:
+            system_prompt: The system prompt that sets the context for the
+                database description.
+            table_description: The description of the table in the database.
+            column_description: The description of the columns in the table.
+                Defaults to None.
+        """
+        if metric_db_id not in self._description_cache["db_description"]:
+            tables_list = [row["name"] for row in json.loads(table_description)]
+            db_schema = await self.tools.get_tables_schema(
+                tables_list, asession, metric_db_id
+            )
+
+            system, prompt = generate_description_prompt(
+                system_prompt=sys_message,
+                tables_description=table_description,
+                db_schema=db_schema,
+                column_description=column_description,
+            )
+            generated_description = await _ask_llm_json(
+                prompt=prompt,
+                system_message=system,
+                llm=self.llm,
+                temperature=self.temperature,
+            )
+            self.logger.debug(
+                f"Generated description for {metric_db_id}: {generated_description}"
+            )
+            self._description_cache["db_description"][
+                metric_db_id
+            ] = generated_description
+
+        return self._description_cache["db_description"][metric_db_id]["answer"][
+            "db_description"
+        ]
+
+    @track_time(create_class_attr="timings")
+    @cached(ttl=60 * 60 * 24)
+    async def generate_suggested_questions(
+        self,
+        asession: AsyncSession,
+        metric_db_id: str,
+        sys_message: str,
+        table_description: str,
+        column_description: str = "",
+    ) -> str:
+        """
+        Generate suggested questions based on the database description.
+
+        Args:
+            system_prompt: The system prompt that sets the context for the
+                suggested questions.
+            table_description: The description of the table in the database.
+            column_description: The description of the columns in the table.
+                Defaults to None.
+        """
+        if metric_db_id not in self._description_cache["suggested_questions"]:
+            tables_list = [row["name"] for row in json.loads(table_description)]
+            db_schema = await self.tools.get_tables_schema(
+                tables_list, asession, metric_db_id
+            )
+
+            system, prompt = generate_suggested_questions_prompt(
+                system_prompt=sys_message,
+                tables_description=table_description,
+                db_schema=db_schema,
+                column_description=column_description,
+            )
+            generated_questions = await _ask_llm_json(
+                prompt=prompt,
+                system_message=system,
+                llm=self.llm,
+                temperature=self.temperature,
+            )
+            self.logger.debug(
+                f"Generated questions for {metric_db_id}:{generated_questions}"
+            )
+            self._description_cache["suggested_questions"][
+                metric_db_id
+            ] = generated_questions
+
+        return self._description_cache["suggested_questions"][metric_db_id]["answer"][
+            "suggested_questions"
+        ]
+
+
+def get_db_descriptor(
+    llm: str = "gpt-4o", temperature: float = 0.1
+) -> DatabaseDescriptor:
+    """
+    Return the DatabaseDescriptor instance.
+
+    Args:
+        llm (str): The name of the LLM model to use. Defaults to "gpt-4o".
+        temperature (float): The temperature to use when generating descriptions.
+            Defaults to 0.1.
+    """
+    global _db_descriptor_instance
+    if _db_descriptor_instance is None:
+        _db_descriptor_instance = DatabaseDescriptor(llm=llm, temperature=temperature)
+    return _db_descriptor_instance
---
+++ askametric/query_processor/db_descriptor/descriptor_prompts.py
@@ -0,0 +1,86 @@
+def generate_description_prompt(
+    system_prompt: str,
+    tables_description: str,
+    db_schema: str,
+    column_description: str = "",
+) -> tuple[str, str]:
+    """
+    Create prompt to get DB description
+    """
+    system = """
+    You are an expert in semantics and contextualization.
+    Provide a DESCRIPTION of the information available in the database,
+    do NOT provide specific numbers.
+    Be clear and descriptive, but write a description with 150 WORDS OR LESS.
+    Use bullet points and lists, to make for easier reading.
+    """
+    prompt = f"""
+    ===== User input description =====
+    {system_prompt}
+
+    ===== Database tables =====
+    {tables_description}
+
+    ===== Database columns (may be empty)=====
+    {column_description}
+
+    ===== Database schema =====
+    {db_schema}
+
+
+    ==== Database Description ====
+    Based on the user input description, tables, columns and schema, produce a SUMMARY
+    that answers the following questions:
+    What type of information is present in the database?
+    What information is NOT in the database?
+
+    ==== Answer Format ====
+    python parsable json with key "db_description" and a string as the description.
+    """
+
+    return system, prompt
+
+
+def generate_suggested_questions_prompt(
+    system_prompt: str,
+    tables_description: str,
+    db_schema: str,
+    column_description: str = "",
+) -> tuple[str, str]:
+    """
+    Create prompt to get suggested questions
+    """
+    system = """
+    You are a technical advisor to non-technical decision-makers.
+    Your job is to suggest SIMPLE questions these officials can ask based on the data
+    available in the database, to access information that can help them make better
+    decisions.
+    These questions MUST be answerable using the information available in the database.
+    """
+    prompt = f"""
+    ===== User input description =====
+    {system_prompt}
+
+    ===== Database tables =====
+    {tables_description}
+
+    ===== Database columns (may be empty)=====
+    {column_description}
+
+    ===== Database schema =====
+    {db_schema}
+
+
+    ==== Suggested Questions ====
+    Based on the user input description, tables, columns, and schema,
+    provide a list of 5 NEW QUESTIONS that can be answered using the information
+    available in the database.
+    These questions should be SIMPLE, and NOT ask for correlation, relationship, etc.
+    Direct comparisons between datapoints or aggregated data are allowed.
+
+    ==== Answer Format ====
+    python parsable json with key "suggested_questions" and list of questions
+    as a string.
+    """
+
+    return system, prompt
