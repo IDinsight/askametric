70e9cdd5d7871738929ab46be05228fd685c3426
ziakhan04 <zia.khan@idinsight.org>
2025-04-01 16:15:19 +0530

    modify ask_llm_json to add json prompt to system prompt

--- askametric/utils.py
+++ askametric/utils.py
@@ -58,6 +58,37 @@ def setup_logger(
 llm_call_logger = setup_logger("LLM_call")
 
 
+def enforce_json_format(system_message: str) -> str:
+    """
+    Modify the system message to JSON format in the LLM response by modifying
+    the input system message.
+    """
+
+    force_to_json_prompt = """
+    Respond ONLY in a python parsable JSON format.
+    Do not include any extra text, explanations, or markdown formatting.
+
+    This is wrong -
+    ```json
+    {
+        "key_1": "value_1",
+        "key_2": "value_2"
+    }
+    ```
+
+    This is correct -
+    {
+        "key_1": "value_1",
+        "key_2": "value_2"
+    }
+    """
+    # Append the JSON enforcement prompt only if it's not already present
+    if force_to_json_prompt.strip() not in system_message:
+        modified_sys_message = f"{system_message}\n{force_to_json_prompt}"
+
+    return modified_sys_message
+
+
 @cached(ttl=60 * 60 * 24)
 @retry(
     stop=(stop_after_attempt(3) | stop_before_delay(10)),
@@ -78,19 +109,18 @@ async def ask_llm_json(
         system_message (str): The system message to ask the LLM model
     """
     llm_call_logger.debug(f"LLM input: 'model': {llm}, 'messages': {prompt}")
+    sys_message = enforce_json_format(system_message)
     response = await acompletion(
         model=llm,
         temperature=temperature,
         messages=[
-            {"content": system_message, "role": "system"},
+            {"content": sys_message, "role": "system"},
             {"content": prompt, "role": "user"},
         ],
-        response_format={"type": "json_object"},
         api_key=api_key,
     )
 
     cost = completion_cost(response)
-
     result = {
         "answer": json.loads(response.choices[0].message.content),
         "cost": cost,
